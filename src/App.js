38178c784be0c7d87c727b5603de64e5511e8fe7
piyushsingh000 <Kamalc218@gmail.com>
2022-11-03 16:30:37 +0530

    PUSH

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState } from "react";
+import "./App.css";
+import TodosList from "./components/TodosList";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import axios from "axios";
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  const baseURL =
+    "https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5";
+
+  const fetch = () =>
+    axios.get(baseURL).then((response) => {
+      console.log("HELLO");
+      setTodos(response.data);
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+          <div className="fetching">
+            <button className="button-add2" onClick={fetch}>
+              Fetch
+            </button>
+          </div>
+        </div>
+        <div>
+          <TodosList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+      </div>
     </div>
   );
 }
